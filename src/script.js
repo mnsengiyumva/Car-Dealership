7a17da037691184c4b3352a93a915c630a3a4a0a
mnsengiyumva <nsengiyumvamico@gmail.com>
2026-02-11 22:10:35 -0400

    Day 11

--- src/script.js
+++ src/script.js
@@ -4,3 +4,8 @@ const link = document.querySelector(".last");
 link.addEventListener("click", function(){
     alert("You clicked the link");
 })
+
+const lin = document.querySelector(".section1");
+lin.addEventListener("click", function(){
+    alert("Clicked");
+})
