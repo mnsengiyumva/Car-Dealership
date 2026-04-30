548c605ae43a9e4e0a55e3a224e705c21ff73d6b
mnsengiyumva <nsengiyumvamico@gmail.com>
2026-04-29 21:49:00 -0300

    favorites page

---
+++ src/favorites.js
@@ -0,0 +1,19 @@
+let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+const box = document.getElementById("favoriteCars");
+
+if(favorites.length === 0){
+    box.innerHTML = "<h2> No Favorite cars yet. </h2>";
+} else{
+    favorites.forEach(car => {
+        box.innerHTML += `<div class = "fav-card">
+                            <h2> ${car} </h2>
+                        </div>
+                        `;
+    });
+    
+}
+
+function removeFav(id){
+    
+}
