3238b5650f0be81e6f2e8d0e0137bb3e2535ab07
mnsengiyumva <nsengiyumvamico@gmail.com>
2026-05-17 21:34:53 -0300

    Car features

--- src/script.js
+++ src/script.js
@@ -136,7 +136,7 @@ evBtn.addEventListener("click", function(){
 
 document.addEventListener("click", function(event){
     if(!evBtn.contains(event.target) && !evCt.contains(event.target)){
-        evCt.classList.add("hidev")
+        evCt.classList.add("hideEv")
     }
 })
 
