7cb95d03716bd2f18c05ea311e5881f0d2d60ee4
mnsengiyumva <nsengiyumvamico@gmail.com>
2026-09-18 15:21:00 -0300

    backend router

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,12 @@ router.post('/signup', async (req, res) => {
         const hashed = await bcrypt.hash(password, 10);
         const user = await User.create({name, email, password: hashed});
 
-        const tiken = jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
-        res.json({token, user: {id: user._id, name:user.name, email: user.email}})
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        res.json({token, user: {id: user._id, name:user.name, email: user.email}});
+
     }
-})
+
+    catch{
+        res.status(500).json({message: 'Server error'})
+    }
+});
