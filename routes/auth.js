7c94a2ec4631c6dbb4d8e46104e03f96f64ebb32
mnsengiyumva <nsengiyumvamico@gmail.com>
2026-09-18 15:07:05 -0300

    Backend Router

--- routes/auth.js
+++ routes/auth.js
@@ -4,3 +4,10 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
+
+router.post('/signup', async (req, res) => {
+    try{
+        const {name, email, password} = req.body;
+        const exists = await User.findOne({email});
+    }
+})
