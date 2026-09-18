5d40cb1763b957ebd1c5b50c3f42b8105fda5a0e
mnsengiyumva <nsengiyumvamico@gmail.com>
2026-09-17 23:05:04 -0300

    backend server

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const mongoose= require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-application.use(cors());
-application.use(express.json());
-application.use(express.static('public'));
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/cars', require('./routes/cars'))
+app.use('/api/cart', require('./routes/cart'))
+
+
+mongoose.connect(process.env.MONGO_URI).then(( => {
+    console.log(MongoDB connected)
+}))
