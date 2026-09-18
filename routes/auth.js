7567c60d0c8169cba8f1f9627a3fb6a5fbf79fac
mnsengiyumva <nsengiyumvamico@gmail.com>
2026-09-18 15:19:04 -0300

    backend router

--- routes/auth.js
+++ routes/auth.js
@@ -9,5 +9,13 @@ router.post('/signup', async (req, res) => {
     try{
         const {name, email, password} = req.body;
         const exists = await User.findOne({email});
+
+        if(exists) return res.status(400).json({message: 'Email already registered'});
+
+        const hashed = await bcrypt.hash(password, 10);
+        const user = await User.create({name, email, password: hashed});
+
+        const tiken = jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        res.json({token, user: {id: user._id, name:user.name, email: user.email}})
     }
 })
