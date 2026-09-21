a319eed8953bf7308285eb915beb8495f01e1fe2
mnsengiyumva <nsengiyumvamico@gmail.com>
2026-09-20 22:37:57 -0300

    Router AI chatbot js code

--- routes/chat.js
+++ routes/chat.js
@@ -5,4 +5,30 @@ const Anthropic = require('@anthropic-ai/sdk')
 const client = new Anthropic({apiKey: process.ANTHROPIC_API_KEY});
 
 
-rou
+router.post('/', async(req, res) => {
+
+    const{messages} = req.body;
+
+    try{
+
+        const response = await client.messages.create({
+            model: 'claude-sonnet-4-6',
+            max_tokens = 1000,
+            system = `You are helpful Luxury car asistant for Infinity luxuries..`,
+            messages
+        });
+
+        res.json({reply: response.content[0].text})
+
+
+    } 
+    catch(err){
+
+        res.status(500).json({message: 'Chat error'})
+
+    }
+})
+
+module.exports = router
+
+
